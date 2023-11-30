f079002622b7725c61b9d8a5be8dd90b31ee7317
Nadia Hina <nadianadia062@gmail.com>
2023-11-29 17:54:31 -0800

    Completed elif statments for various operations

--- calculator.py
+++ calculator.py
@@ -3,19 +3,42 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
-run = True
 
-while run:
+
+while True:
     inp_string = input('Enter your expression: ')
     tokens = inp_string.split(' ')
-
+    
     for token in tokens:
         if token == 'q':
-            run = False
             print("Goodbye.")
             break
-        elif len(tokens) < 3:
-            input("Please enter your prefix followed by two integers:")
+    if len(tokens) < 3:
+        input("Please enter your prefix followed by two integers:")
+
+    result = None
+    num1 = tokens[1]
+    num2 = tokens[2]
+
     
     if tokens[0] == '+':
-        result = add(token[1], token[2])
+        result = add(float(num1), float(num2))
+    elif tokens[0] == '-':
+        result = subtract(num1, num2)
+    elif tokens[0] == '*':
+        result = multiply(num1, num2)
+    elif tokens[0] == '/':
+        result = divide(num1, num2)
+    elif tokens[0] == 'square':
+        result = square(num1)
+    elif tokens[0] == 'cube':
+        result = cube(num1)
+    elif tokens[0] == 'pow':
+        result = power(num1, num2)
+    elif tokens[0] == 'mod':
+        result = mod(num1, num2)
+    print(result)
+
+
+
+
