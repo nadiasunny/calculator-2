7719a2ce58b026d36fa91bba726324f22d56651e
Nadia Hina <nadianadia062@gmail.com>
2023-11-29 17:11:18 -0800

    Accounted for string length & precense of 'q'

--- calculator.py
+++ calculator.py
@@ -3,5 +3,19 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
+run = True
 
-# Replace this with your code
+while run:
+    inp_string = input('Enter your expression: ')
+    tokens = inp_string.split(' ')
+
+    for token in tokens:
+        if token == 'q':
+            run = False
+            print("Goodbye.")
+            break
+        elif len(tokens) < 3:
+            input("Please enter your prefix followed by two integers:")
+    
+    if tokens[0] == '+':
+        result = add(token[1], token[2])
