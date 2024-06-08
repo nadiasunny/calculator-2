451cee24bad4572f16d35ba52a0b12556d38620a
Nadia Hina <nadianadia062@gmail.com>
2024-06-08 00:01:23 -0700

    added elif statments to check operation

--- calculator.py
+++ calculator.py
@@ -8,35 +8,34 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 while True:
     inp_string = input('Enter your expression: ')
     tokens = inp_string.split(' ')
+    result = None
+    num1 = tokens[1]
+    num2 = tokens[2]
     
     for token in tokens:
         if token == 'q':
             print("Goodbye.")
             break
-    if len(tokens) < 3:
-        input("Please enter your prefix followed by two integers:")
-
-    result = None
-    num1 = tokens[1]
-    num2 = tokens[2]
+    if len(tokens) < 2:
+        num2 = 0
 
     
     if tokens[0] == '+':
         result = add(float(num1), float(num2))
     elif tokens[0] == '-':
-        result = subtract(num1, num2)
+        result = subtract(float(num1), float(num2))
     elif tokens[0] == '*':
-        result = multiply(num1, num2)
+        result = multiply(float(num1), float(num2))
     elif tokens[0] == '/':
-        result = divide(num1, num2)
+        result = divide(float(num1), float(num2))
     elif tokens[0] == 'square':
-        result = square(num1)
+        result = square(float(num1))
     elif tokens[0] == 'cube':
-        result = cube(num1)
+        result = cube(float(num1))
     elif tokens[0] == 'pow':
-        result = power(num1, num2)
+        result = power(float(num1), float(num2))
     elif tokens[0] == 'mod':
-        result = mod(num1, num2)
+        result = mod(float(num1), float(num2))
     print(result)
 
 
